abfd96f919d0ea74613f972ba53d7a2c92181647
brianrego03 <brian.rego03@gmail.com>
2025-03-15 14:50:10 +0530

    Implement router structure in App and Dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import router from './components/Routers'
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <RouterProvider router={router} />;
       
     </>
   )
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,10 +4,24 @@ const Dashboard = ()=>{
 
 
     return (
+        <>
         <p>Dashboard</p>
-        
+        <nav>
+            <div>
+                <Link to="/">Home</Link>
+            </div>
+            <div>
+                <Link to="/catalog">Catalog</Link>
+
+            </div>
+            <div>
+                <Link to="/checkout">Cart</Link>
+            </div>    
+        </nav>
+        <Outlet />
 
 
+        </>
     )
 }
 
