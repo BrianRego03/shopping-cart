c66a3730e68f4b5e2b6d40e2e1cfc272ec46712e
Brian rego <brian.rego03@gmail.com>
2025-03-18 18:54:48 +0530

    Create a custom hook in dashboard to pull API data

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,36 @@
+import { useEffect } from "react";
 import { Link,Outlet, useNavigate } from "react-router-dom";
 
 const Dashboard = ()=>{
 
     const navigate = useNavigate();
+    const productArray=[];
+
+    const useFetchProducts =()=>{
+        useEffect(()=>{
+            const fetchData= async ()=>{
+                try{
+                    const response = await fetch("https://fakestoreapi.com/products");
+                    if(!response.ok){
+                        throw new Error('HTTP error');
+
+                    }
+                    let postData=await response.json();
+                    console.log(postData);
+                }
+                catch(err){
+                    console.log(err.message);
+
+                }
+                finally{
+                    console.log("done");
+                }
+            }
+            fetchData();
+        },[]);
+
+    }
+    useFetchProducts();
 
 
     return (
