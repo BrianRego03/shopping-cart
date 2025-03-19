91c3eb90274d78aa52855a400dca0e532d04f1d0
brianrego03 <brian.rego03@gmail.com>
2025-03-19 17:38:44 +0530

    Format css of product card

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,5 +1,6 @@
 import { useOutletContext } from "react-router-dom";
 import ProductCard from "./Productcard";
+import "../styles/catalog.css"
 
 const Catalog = ()=>{
     const {productArray}=useOutletContext();
@@ -9,9 +10,12 @@ const Catalog = ()=>{
 
     return (
         <>
-            {productArray.map((item,index)=>{
-                return <ProductCard key={index} product={item}/>
-            })}
+            <div id="productCatalog">
+                {productArray.map((item,index)=>{
+                    return <ProductCard key={index} product={item}/>
+                })}
+            </div>
+            
             <p>Catalog Page</p>
         </>
     )
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,17 @@
-
+import  '../styles/productCard.css'
 
 const ProductCard=({product})=>{
-    console.log(product);
+    
 
+    return(
+        <>
+            <div className="cardStyle">
+                <div className='imageContainer'><img src={product.image}></img></div>
+                <p>{product.title}</p>
+                <p className='buttonDivision'>+/-</p>
+            </div>
+        </>
+    )
 
 }
 
