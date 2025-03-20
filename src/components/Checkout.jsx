57f0ad695deb67f9978c403c0de70fc606230dda
Brian rego <brian.rego03@gmail.com>
2025-03-21 00:20:48 +0530

    Create basic structure and logic for checkout page and create checkout card component

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,9 +1,20 @@
+import { useOutletContext } from "react-router-dom";
+import CheckoutCard from "./CheckoutCard";
 
 
 const Checkout = ()=>{
+    const {productArray,addToCart}=useOutletContext();
 
 
-    return (<p>Checkout Page</p>)
+    return (
+    
+        <>
+            <p>Checkout Page</p>
+            {productArray.filter(item=>item.quantity>0)
+                        .map((item)=><CheckoutCard key={item.id} product={item}/>)}
+        </>
+
+    )
 }
 
 export default Checkout;
---
+++ src/components/CheckoutCard.jsx
@@ -0,0 +1,14 @@
+
+
+
+const CheckoutCard=({product})=>{
+
+    return(
+        <>
+            <div>{product.title}:{product.quantity}</div>
+        </>
+    )
+
+}
+
+export default CheckoutCard;
