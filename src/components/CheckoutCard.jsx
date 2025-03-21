21a331fe1927d1364dfc0727ef91f546daaa9606
brianrego03 <brian.rego03@gmail.com>
2025-03-21 17:47:29 +0530

    Apply styling to checkout card

--- src/components/CheckoutCard.jsx
+++ src/components/CheckoutCard.jsx
@@ -1,4 +1,4 @@
-
+import "../styles/checkoutCard.css"
 
 
 const CheckoutCard=({product,updateCart})=>{
@@ -16,15 +16,30 @@ const CheckoutCard=({product,updateCart})=>{
 
     return(
         <>
-            <div>
-                {product.title}
+            <div className="checkoutContainer">
+                <div><img className="checkoutImage" src={product.image}></img></div>
+                <div className="checkoutSection">
+                    <div className="checkoutTitle">
+                        {product.title}
+                    </div>
+                    <div className="buttonSection"> 
+                        <div className="quantityChange">
+                            <button className="decrement" onClick={decrementItem}>-</button>
+                            <span>{product.quantity}</span>
+                            <button className="increment" onClick={incrementItem}>+</button>
+                            
+                        </div> 
+                        <div>
+                            <button className="deleteButton" onClick={deleteItem}>Delete</button>
+                            
+                        </div>  
+                        
+                            
+                    </div>
+
+                </div>
             </div>
-            <div>    
-                <button onClick={decrementItem}>-</button>
-                {product.quantity}
-                <button onClick={incrementItem}>+</button>
-                <button onClick={deleteItem}>Delete</button>
-            </div>
+            
         </>
     )
 
