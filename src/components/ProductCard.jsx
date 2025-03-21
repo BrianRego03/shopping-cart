ca0a5cfe65ec0f41d1ebe91422df3bb1cd7147d3
Brian rego <brian.rego03@gmail.com>
2025-03-21 08:17:46 +0530

    Remove unnecessary states from product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,26 +1,16 @@
-import { useState } from 'react'
 import  '../styles/productCard.css'
 
 const ProductCard=({product,cartUpdate})=>{
-    const [cardQuantity,setCardQuantity]=useState(product.quantity);
     const decreaseCardQuantity=()=>{
-        if(cardQuantity===0){
-            return;
-        }
-        else{
-            setCardQuantity(itemNumber=>itemNumber-1);
             cartUpdate(product.id,product.quantity-1);
 
-        }
     }
 
     const initialCardQuantity=()=>{
-        setCardQuantity(1);
         cartUpdate(product.id,1);
 
     }
     const increaseQuantity=()=>{
-        setCardQuantity(itemNumber=>itemNumber+1);
         cartUpdate(product.id,product.quantity+1);
 
 
@@ -33,9 +23,9 @@ const ProductCard=({product,cartUpdate})=>{
                 <div className='imageContainer'><img src={product.image}></img></div>
                 <p>{product.title}</p>
                 {
-                    cardQuantity>0?(<div className='buttonDivision'>
+                    product.quantity>0?(<div className='buttonDivision'>
                         <button onClick={decreaseCardQuantity}>-</button>
-                        <span>{cardQuantity}</span>
+                        <span>{product.quantity}</span>
                         <button onClick={increaseQuantity}>+</button>
                     </div>):(<div>
                     <button onClick={initialCardQuantity}>Add to cart</button>
